126081617407ee9af8a91db36f24a66c1f8d193a
KingB94 <beneholzner@outlook.de>
2025-04-29 15:49:49 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/eins')
+def eins():
+    return render_template('eins.html')
+
+@app.route('/zwei')
+def zwei():
+    return render_template('zwei.html')
+
+@app.route('/drei')
+def drei():
+    return render_template('drei.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
