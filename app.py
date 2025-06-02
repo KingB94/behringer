1eb3f569b6b0bdc7b0ff22d8477bcca859ee546b
KingB94 <beneholzner@outlook.de>
2025-06-02 13:30:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/eins')
-def eins():
-    return render_template('eins.html')
-
-@app.route('/zwei')
-def zwei():
-    return render_template('zwei.html')
-
-@app.route('/drei')
-def drei():
-    return render_template('drei.html')
-
-@app.route('/vier')
-def vier():
-    return render_template('vier.html')
-
-@app.route('/fuenf')
-def fuenf():
-    return render_template('fuenf.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
